c21fb7260acecd83508a41e622b0f115e182f162
Alec Helbling <stahelbling1@ppsstudents.net>
2017-05-04 15:56:51 -0400

    refactoring

--- test/image_net_test.py
+++ test/image_net_test.py
@@ -2,5 +2,5 @@ import image
 
 gen=image.ImageGenerator("../caffe/models/bvlc_googlenet/bvlc_googlenet.caffemodel","../caffe/models/bvlc_googlenet/deploy.prototxt","data","loss3/classifier","prob")
 
-gen.generate_number_image(224+10,224+10,5,31,"imagenet/ouput1.jpg")
+gen.generate_image_iterations(224+10,224+10,5,31,"imagenet/ouput1.jpg")
 
--- test/test2.py
+++ test/test2.py
@@ -2,5 +2,5 @@ import image
 
 gen=image.ImageGenerator("../caffe/examples/cifar10_copy/cifar10_full_iter_70000.caffemodel.h5","../caffe/examples/cifar10_copy/cifar10_full.prototxt","data","ip1","prob")
 
-gen.generate_number_image(32,32,1000,8,"ouput.jpg")
+gen.generate_image_iterations(64,64,2,8,"ouput.jpg")
 
